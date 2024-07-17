dea5e12713cb95e37291e4ffe374a3b731a94d91
shadowsnavi <kumori.k.kuro@gmail.com>
2024-07-17 16:07:11 -0700

    complete

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    return Math.abs(blocks-42);
+}
+function distanceFromHqInFeet(blocks) {
+    return Math.abs(blocks-42) * 264;
+}
+function distanceTravelledInFeet(num1, num2) {
+    return Math.abs(num1 - num2) * 264;
+}
+function calculatesFarePrice (num1,num2){
+
+    let feet = ((Math.abs(num1 - num2)) * 264)
+    if (feet < 400) {
+        return 0
+    } 
+    else if (feet < 2000) {
+        return ((feet -400) * 0.02)
+    }
+    else if (feet > 2000 && feet < 2500) {
+        return 25
+    }
+    else {
+        return 'cannot travel that far'
+    }
+}
